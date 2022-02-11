f9ea3cd4e45653e5108a0c88141a5eea282fa702
someshsingh22 <singhksomesh@gmail.com>
2022-02-11 16:29:09 +0530

    Dataloader

---
+++ TDA_KE/data/__init__.py
@@ -0,0 +1,6 @@
+dataset_folders = {
+    "INSPEC": "Inspec",
+    "SE-2010": "processed_semeval-2010",
+    "SE-2017": "SemEval-2017",
+}
+from KP_Dataloader import get_dataloader as get_KP_dataloader
